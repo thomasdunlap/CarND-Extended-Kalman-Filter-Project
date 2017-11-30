c0f199ff4f820a7ced8b40d2d52c2c3f8f4c8c3b
thomasdunlap <tom.a.dunlap@gmail.com>
2017-11-30 12:36:00 -0500

    Updated Update() kfcpp.

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -27,10 +27,18 @@ void KalmanFilter::Predict() {
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Kalman Filter equations
-  */
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H) * P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
